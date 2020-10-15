b9df49081c21cc1862a623254829e7bc1f33f906
535803710 <535803710@qq.com>
2020-10-15 14:36:22 +0800

    U:新功能

---
+++ miniprogram/pages/uploadSticker/uploadSticker.js
@@ -0,0 +1,54 @@
+// miniprogram/pages/uploadSticker/uploadSticker.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    arr: [
+      "https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/tx4.jpg?sign=9817258738b68c534b35fbfc04bde928&t=1553372736",
+      "https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/tx2.jpg?sign=6e1e8eec2c2fc497e3b2ac03b367e770&t=1553372700",
+      "https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/tx3.jpg?sign=32821196ccfd12115af3d64dc7d67132&t=1553372724",
+      "https://656e-env-9eb476-1258886794.tcb.qcloud.la/images/tx1.jpg?sign=ed14431dbf86a4d143841e695deaa9b2&t=1553372672",
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {},
+});
